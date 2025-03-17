40f75b842f2396533a0023f29429907a4e615db7
Jannis Haendke <7644955+jhaendke@users.noreply.github.com>
2025-03-17 06:03:01 +0100

    KrippendorffsAlpha (heval3-heval4, heval3-meval) + Plots

---
+++ scripts/analysis2_krippLLM.R
@@ -0,0 +1,205 @@
+### Environment
+# getwd()
+# setwd("")
+# Sys.getenv("VAR1")
+# rm(list = ls())  # clean global env
+# install.packages("")
+
+rm(list = ls())
+
+library(tidyverse)
+library(openxlsx)
+library(irr)
+
+### IMPORT
+
+df <- read.csv(
+    "data/processed/4th/MASTER_combined_4th_DIFFS.csv",
+    header = TRUE,
+    sep = ",",
+    na.strings = c("", "NA"),
+    fill = TRUE, # fill empty cells with NA
+    stringsAsFactors = FALSE
+)
+# heval <- read.csv(
+#     "data/processed/4th/MASTER_human-eval_4th_DIFFS.csv",
+#     header = TRUE, # without headers
+#     sep = ",",
+#     na.strings = c("", "NA"),
+#     fill = TRUE, # fill empty cells with NA
+#     stringsAsFactors = FALSE
+# )
+# meval <- read.csv(
+#     "data/processed/MASTER_machine-eval_DIFFS.csv",
+#     header = TRUE, # without headers
+#     sep = ",",
+#     na.strings = c("", "NA"),
+#     fill = TRUE, # fill empty cells with NA
+#     stringsAsFactors = FALSE
+# )
+
+df_long <- df
+# df_names <- colnames(df)
+df_labels <- c(
+    "Source",
+    "Evaluator name",
+    "Dataset",
+    "Datapoint",
+    "Summary: Consistency",
+    "Article: Coalition Breakdown",
+    "Article: SPD",
+    "Article: FDP",
+    "Article: Greens",
+    "Article: Scholz",
+    "Article: Lindner",
+    "Article: Habeck",
+    "Summary: Coalition Breakdown",
+    "Summary: SPD",
+    "Summary: FDP",
+    "Summary: Greens",
+    "Summary: Scholz",
+    "Summary: Lindner",
+    "Summary: Habeck",
+    "Comment",
+    "Datapoint_new",
+    "Diff: Coalition Breakdown",
+    "Diff: SPD",
+    "Diff: FDP",
+    "Diff: Greens",
+    "Diff: Scholz",
+    "Diff: Lindner",
+    "Diff: Habeck"
+)
+
+### PREP
+
+# Set levels, data types
+
+# Make Sentiment pairs (atc_*, smm_*)
+
+
+### ANALYZE
+
+# calc Cohen's Kappa
+
+# calc Spearman's Coef
+
+# calc Krippendorf's Alpha
+
+# Krippendorff's Alpha
+#
+# select: evaluator, atc_spd, datapoint_new
+
+# Intercoder reliability (ICR)
+
+### TRANSFORM to LONG FORMAT
+
+df <- df_long
+
+df <- df %>%
+    filter(source %in% c("heval3", "meval")) %>% # select eligible
+    select(evaluator, matches("^(atc_|smm_)"), datapoint_new)
+
+df_names_short <- colnames(df)[str_detect(colnames(df), "^(atc_|smm_)")]
+
+df <- df %>%
+    group_by(datapoint_new) %>% # case identifier
+    filter(n() == 2) %>% # only cases with 2 coders
+    arrange(desc(datapoint_new)) %>% # sort
+    ungroup()
+
+
+### TRANSFORM to WIDE FORMAT
+
+variables <- df_names_short # Variables to analyze
+
+results <- list() # Empty list to store results
+alpha_values <- numeric(length(variables))
+names(alpha_values) <- variables
+
+# Loop through each variable
+for (i in 1:length(variables)) {
+    var <- variables[i]
+
+    # for (var in variables) {
+    # Create matrix for the current variable
+    var_matrix <- df %>%
+        select(datapoint_new, evaluator, all_of(var)) %>%
+        pivot_wider(
+            id_cols = datapoint_new,
+            names_from = evaluator,
+            values_from = all_of(var)
+        ) %>%
+        select(-datapoint_new) %>%
+        mutate(across(everything(), as.character))
+
+    # Transpose the matrix
+    var_matrix <- var_matrix %>%
+        t() %>%
+        as.matrix()
+
+    # Calculate Krippendorff's Alpha
+    alpha_result <- kripp.alpha(var_matrix, method = "ordinal")
+
+    # Store the result
+    results[[var]] <- alpha_result
+    alpha_values[i] <- alpha_result$value
+
+    # Print the result
+    cat("\nResults for", var, ":\n")
+    print(alpha_result)
+}
+
+print(alpha_values)
+
+# Convert the vector to a df
+alpha_table <- data.frame(
+    variable = names(alpha_values),
+    value = as.numeric(alpha_values),
+    stringsAsFactors = FALSE
+)
+
+# Understand results
+#   Missing Data (NA):
+#   > If an item (datapoint) has missing ratings from
+#   all evaluators or only one rating, it might
+#   be excluded from the calculation.
+#   > Krippendorff requires at least two valid
+#   ratings to assess agreement for a datapoint.
+
+# IRC results "atc_scholz":
+#   Subjects: 15
+#   Raters: 8
+#   alpha: 0.648
+
+results[["atc_scholz"]] # Access results
+
+write.csv(alpha_table, "output/alpha_results_krippLLM.csv", row.names = FALSE)
+
+write.xlsx(alpha_table,
+    file = "output/output/alpha_results_krippLLM.xlsx",
+    asTable = FALSE,
+    overwrite = TRUE,
+    colNames = TRUE
+)
+
+### Non-loop implementation
+#
+# atc_scholz_matrix <- df %>%
+#     select(datapoint_new, evaluator, atc_scholz) %>%
+#     pivot_wider(
+#         id_cols = datapoint_new,
+#         names_from = evaluator,
+#         values_from = atc_scholz
+#     ) %>%
+#     select(-datapoint_new) %>%
+#     mutate(across(everything(), as.character))
+#     # as.matrix()
+#
+# atc_scholz_matrix <- atc_scholz_matrix %>%
+#     t() %>%  # transpose
+#     as.matrix()
+#
+# atc_scholz_alpha <- kripp.alpha(atc_scholz_matrix, method = "ordinal")
+#
+# print(atc_scholz_alpha)
--- scripts/analysis2_viz.R
+++ scripts/analysis2_viz.R
@@ -5,4 +5,154 @@
 # rm(list = ls())  # clean global env
 # install.packages("")
 
-### Add Diffs to datasets
+rm(list = ls())
+
+library(tidyverse)
+library(openxlsx)
+library(irr)
+library(ggplot2)
+library(svglite)
+
+### IMPORT
+df_icr <- read.csv(
+    "output/alpha_results_krippICR.csv",
+    header = TRUE,
+    sep = ",",
+    na.strings = c("", "NA"),
+    fill = TRUE, # fill empty cells with NA
+    stringsAsFactors = FALSE
+)
+df_llm <- read.csv(
+    "output/alpha_results_krippLLM.csv",
+    header = TRUE,
+    sep = ",",
+    na.strings = c("", "NA"),
+    fill = TRUE, # fill empty cells with NA
+    stringsAsFactors = FALSE
+)
+
+### VISUALIZATIONS
+
+# df_icr_atc <- df_icr %>%
+#     filter(str_detect(variable, "atc_"))
+
+# df_icr_smm <- df_icr %>%
+#     filter(variable != "smm_consistency") %>%
+#     filter(str_detect(variable, "smm_"))
+
+
+# Bar chart
+# ggplot(df_icr_atc, aes(x = variable, y = value, fill = variable)) +
+#     geom_bar(stat = "identity", width = 0.6) +
+#     # scale_fill_brewer(palette = "Blues") +
+#     labs(
+#         title = "Krippendorff's Alpha by Variable",
+#         x = "Variable",
+#         y = "Krippendorff's Alpha"
+#     ) +
+#     geom_text(aes(label = round(value, 3)), vjust = -0.5) +
+#     ylim(0, 1) +
+#     theme_minimal() +
+#     theme(legend.position = "none")
+
+
+# Lollipop
+# ggplot(df_icr_atc, aes(x = variable, y = value, color = variable)) +
+#     geom_segment(aes(x = variable, xend = variable, y = 0, yend = value),
+#         size = 1.5
+#     ) +
+#     geom_point(size = 5) +
+#     geom_text(aes(label = round(value, 3)), vjust = -1) +
+
+#     # scale_color_brewer(palette = "Set2") +
+#     labs(
+#         title = "Krippendorff's Alpha by Variable",
+#         x = "Variable",
+#         y = "Krippendorff's Alpha"
+#     ) +
+#     ylim(0, 1) +
+#     theme_minimal() +
+#     theme(legend.position = "none")
+
+
+# # aggregate
+# ggplot(df_icr, aes(y = value)) +
+#     geom_boxplot(fill = "lightblue") +
+#     theme_minimal() +
+#     labs(
+#         title = "Boxplot of Values",
+#         y = "Value"
+#     )
+
+
+# BOXPLOTS
+
+# for ICR
+plot_data <- df_icr %>%
+    filter(str_detect(variable, "^smm_|^atc_")) %>%
+    mutate(group = case_when(
+        str_detect(variable, "^smm_") ~ "Summary",
+        str_detect(variable, "^atc_") ~ "Article",
+        TRUE ~ NA_character_
+    ))
+
+boxp <- ggplot(plot_data, aes(x = group, y = value)) +
+    geom_boxplot(outlier.size = 3) +
+    scale_y_continuous(limits = c(NA, 1)) + # min, max
+    # stat_boxplot(geom = "errorbar", width = 0.15) +
+    scale_fill_grey(start = 0.6, end = 0.2) +
+    # scale_fill_brewer(palette = "Set1") +
+    labs(
+        title = "Krippendorff's Alpha: Human vs Human,\nby Document Type\n(Intercoder Reliability)",
+        x = "",
+        y = "K. Alpha"
+    ) +
+    theme_minimal() +
+    theme(
+        plot.title = element_text(
+            hjust = 0.5, # Center title (0.5 = center)
+            size = 16,
+            face = "bold"
+        ),
+        axis.text.x = element_text(size = 12, face = "bold"),
+        axis.text.y = element_text(size = 12),
+        legend.position = "none"
+    )
+
+ggsave("output/plot_alpha_irc_boxp.png", plot = boxp, width = 10, height = 7, units = "in", dpi = 300, bg = "white")
+
+
+# for LLM
+
+plot_data <- df_llm %>%
+    filter(str_detect(variable, "^smm_|^atc_")) %>%
+    mutate(group = case_when(
+        str_detect(variable, "^smm_") ~ "Summary",
+        str_detect(variable, "^atc_") ~ "Article",
+        TRUE ~ NA_character_
+    ))
+
+boxp <- ggplot(plot_data, aes(x = group, y = value)) +
+    geom_boxplot(outlier.size = 3) +
+    scale_y_continuous(limits = c(NA, 1)) + # min, max
+    # stat_boxplot(geom = "errorbar", width = 0.15) +
+    scale_fill_grey(start = 0.6, end = 0.2) +
+    # scale_fill_brewer(palette = "Set1") +
+    labs(
+        title = "Krippendorff's Alpha: LLM vs Human,\nby Document Type",
+        x = "",
+        y = "K. Alpha"
+    ) +
+    theme_minimal() +
+    theme(
+        plot.title = element_text(
+            hjust = 0.5, # Center title (0.5 = center)
+            size = 16,
+            face = "bold"
+        ),
+        axis.text.x = element_text(size = 12, face = "bold"),
+        axis.text.y = element_text(size = 12),
+        legend.position = "none"
+    )
+
+ggsave("output/plot_alpha_llm_boxp.png", plot = boxp, width = 10, height = 7, units = "in", dpi = 300, bg = "white")
